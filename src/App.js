0624b2215b1f0b005c9dcb9c76dadff630f64b8a
ANH_VIET <Viet>
2022-06-20 06:57:28 +0700

    fetch error

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import BlogList from "./BlogList";
 
-const url = "http://localhost:8000/data";
+const url = "http://localhost:8000/datas";
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch(url)
         .then((res) => {
+          if (!res.ok) {
+            throw Error("the program is having error");
+          }
           return res.json();
         })
         .then((data) => {
           setData(data);
           setLoading(true);
+        })
+        .catch((error) => {
+          setLoading(true);
+          setError(error.message);
         });
     }, 2000);
   }, []);
@@ -25,6 +33,7 @@ function App() {
   return (
     <div className="App">
       <h1>List of members</h1>
+      {error && <div className="blog_error">{error}</div>}
       {!loading && <p>The List is loading.....</p>}
       {data && <BlogList data={data} />}
     </div>
